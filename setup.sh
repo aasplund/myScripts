d28f25eb2a8c605fd038242cb7eb2b0a56f52044
Anders Asplund <aasplund@gmail.com>
2012-04-13 11:57:31 +0200

    Updating warning message. Added some comments.

--- setup.sh
+++ setup.sh
@@ -1,36 +1,38 @@
 #! /bin/bash
 
+#All scripts in processed in the this script
+scripts=(.bash_gitprompt .bash_profile .bashrc .profile .shellaliases .shellpaths .shellvars .zprofile .zsh .zshenv .zshrc)
+
 setup_symlinks() {
-	scripts=(.bash_gitprompt .bash_profile .bashrc .profile .shellaliases .shellpaths .shellvars .zprofile .zsh .zshenv .zshrc)
 
 	SOURCE="${BASH_SOURCE[0]}"
 	while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 	CURRENT_PATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-	
-	
-	SS_NAME=".shellscripts"
-	ln -sfv $CURRENT_PATH/$SS_NAME $HOME
 
-	OMZ_NAME="oh-my-zsh"
-	ln -sfv $CURRENT_PATH/$OMZ_NAME $HOME
-	rm -f $HOME/.$OMZ_NAME
-	mv -fv $HOME/$OMZ_NAME $HOME/.$OMZ_NAME
-
+#Adding link to oh-my-zsh	
+	ln -sfv $CURRENT_PATH/".oh-my-zsh" $HOME
+	
+#Adding links to scriptfolder and all of its containg scripts
+	SCRIPT_FOLDER_NAME=".shellscripts"
+	ln -sfv $CURRENT_PATH/$SCRIPT_FOLDER_NAME $HOME
 	for script in ${scripts[@]}
 	do
-		ln -svf $SS_NAME/$script $HOME 
+		ln -svf $SCRIPT_FOLDER_NAME/$script $HOME 
 	done
 
-
+#Adding link to project in Dropbox
 	if [ -d $HOME/Dropbox ]
 	then
 		ln -sfv $CURRENT_PATH $HOME/Dropbox
 	fi
 }
-
 echo "
 
 ******************** WARNING! ********************
+
+This script will create symlinks in $HOME with the following names:
+
+${scripts[@]}
 
 If you already have any of the provided script files you should back them up
 in case you want to go back to your original settings.
